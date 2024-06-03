a7af38db5fd00626ed9f1a4f190a651b81d8942f
Yonathan Levi <jonilevi@icloud.com>
2024-06-03 19:22:01 -0400

    fixed type error in cta

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
+import React from "react";
 
-export default function CTA({ className }) {
-  return (
-    <Link href={"https://wa.me/+972507170051"}>
-      <p
-        className={`rounded-xl font-bold px-4 py-3 inline-block bg-primary ${className}`}
-      >
-        Get an Offer Now
-      </p>
-    </Link>
-  );
-}
+const CTA: React.FC<{ className: string }> = ({ className }) => (
+  <Link href={"https://wa.me/+972507170051"}>
+    <p
+      className={`rounded-xl font-bold px-4 py-3 inline-block bg-primary ${className}`}
+    >
+      Get an Offer Now
+    </p>
+  </Link>
+);
+export default CTA;
